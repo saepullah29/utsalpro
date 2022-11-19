2ce2a94bb5af80d8accb789cbe3c9b74450cff90
saepulloh <116586865+saepulloh29@users.noreply.github.com>
2022-11-19 09:47:27 +0700

    Add files via upload

---
+++ Variabel.java
@@ -0,0 +1,13 @@
+
+public class Variabel {
+		public static void main(String[] args) {
+			String var1 = "assalamu'alaikum";
+			String var2 = "Nama Saya : saepulloh";
+			int tahun = 1999;
+			System.out.println(var1);
+			System.out.println(var2);
+			System.out.println("Saya lahir Di Tahun : " + tahun);
+
+		}
+
+	}
